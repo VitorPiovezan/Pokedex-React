29c7bebcf577598e2155c2f551b4e052fef9e6fc
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2020-10-30 16:18:57 -0300

    Criado front do campo de busca, ainda não funcional

--- frontend/pokedex/src/components/Home/home.js
+++ frontend/pokedex/src/components/Home/home.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import logo_pokemon from '../../assets/images/logo_pokemon.png'
 import pokedex from '../../assets/images/pokedex.png'
 
-import { Container, Header, Menu, Imagem, Text } from './styles'
+import { 
+    Container, 
+    Header, 
+    Menu, 
+    Imagem, 
+    Search, 
+    SearchTitle, 
+    Main, 
+    SearchName, 
+    SearchAdvanced,
+    SearchNameInput,
+    Input,
+    DivButtonSearch,
+    ButtonDropdown
+} from './styles'
 
 
 export default function Home() {
@@ -11,14 +24,36 @@ export default function Home() {
     <Container>
         <Header>
             <Menu>
-                <Imagem src={logo_pokemon}/>
+                <Imagem src={pokedex}/>
             </Menu>
         </Header>
 
-
+        <Main>
+            <Search>
+                <SearchName>
+                    <SearchTitle color="#4484CE">Search by Name</SearchTitle>
+                    <Input><SearchNameInput type="text" id="searchName" name="searchName" placeholder="Search for Pokemon"/></Input>
+                
+                </SearchName>
+                <SearchAdvanced>
+                    <SearchTitle color="#707070">Advanced Search</SearchTitle>
+                    <DivButtonSearch>
+                        <ButtonDropdown type="button" name="button" placeholder="Todos Tipos"/>
+                        <ButtonDropdown type="button" name="button" placeholder="Todos Tipos"/>
+                        <ButtonDropdown type="button" name="button" placeholder="Todos Tipos"/>
+                    </DivButtonSearch>
+                </SearchAdvanced>
+            </Search>
+        </Main>
 
     </Container>
 
     );
 }
 
+
+
+
+// <DivButtonSearch>
+// <ButtonDropdown type="button">Todos Tipos</ButtonDropdown>
+// </DivButtonSearch>
--- frontend/pokedex/src/components/Home/styles.js
+++ frontend/pokedex/src/components/Home/styles.js
@@ -3,9 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
 `;
 
+export const Main = styled.main`
+    width: 1000px;
+    margin: auto;
+`
+
+//Header
+
 export const Header = styled.div`
     top: 0;
-    background-color: #4484Ce;
+    background-color: #4484CE;
     height: 80px;
 `
 
@@ -15,20 +22,67 @@ export const Menu = styled.div`
     width: 1000px;
     margin: auto;
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-    
 `
 
 export const Imagem = styled.img`
     height: 60px;
     vertical-align: middle;
+    margin: auto;
 `
 
-export const Text = styled.a`
-    font-size: 25pt;
-    font-weight: bold;
-    color: #f4dc26;
-    text-shadow: #00a -3px -1px;
-    text-decoration: none;
+//Search
+
+export const Search = styled.div`
+    width: 100%;
+    height: 150px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border-bottom: 1px solid #909090;
+`
+
+export const SearchTitle = styled.p`
+    font-size: 12pt;
+    padding: 15px;
+    margin-top: 10px;
+    color: ${props => props.color};
+`
+
+export const SearchName = styled.div`
+    /* background-color: #ccc; */
+    width: 40%;
+    height: 85%;
+`
+
+export const SearchAdvanced = styled.div`
+    /* background-color: #ddd; */
+    width: 500px;
+    height: 85%; 
+`
+
+export const SearchNameInput = styled.input`
+    border-radius:4px;
+    padding: 12px;
+    background: #edf4fc; 
+    border:1px solid #4484ce;
+    width: 80%;
+`
+
+export const Input = styled.div`
+    padding: 0px 15px;
+`
+
+export const DivButtonSearch = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 0px 15px;
+    justify-content: space-between
+`
+
+export const ButtonDropdown = styled.input`
+    border-radius:4px;
+    padding: 12px;
+    background: #707070; 
+    border:1px solid #000000;
+    width: 150px;
 `
